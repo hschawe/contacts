8b959c5f3325c4dffcaa8850de6fb1b89b367f95
Helen Schawe <helen.schawe@gmail.com>
2025-02-26 20:12:35 -0600

    Draw the rest of the owl

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -1,3 +1,4 @@
+from typing import List
 from flask import Flask, render_template, request, jsonify
 import sqlite3
 
@@ -30,9 +31,10 @@ def init_db():
                     )''')
     conn.commit()
 
-    # cursor = conn.execute('INSERT INTO People (first_name, last_name) VALUES ("John", "Smith")')
+    # cursor = conn.execute('INSERT INTO People (first_name, last_name) VALUES ("Helen", "Schawe") RETURNING person_id')
+    # s = cursor.fetchall()
     # conn.commit()
-    # cursor = conn.execute('INSERT INTO Emails (person_id, email) VALUES (2, "helen.schawe@gmail.com")')
+    # cursor = conn.execute(f'INSERT INTO Emails (person_id, email) VALUES ({s[0][0]}, "helen.schawe@gmail.com")')
     # conn.commit()
     # cursor = conn.execute('INSERT INTO Emails (person_id, email) VALUES (3, "johnsmith@gmail.com")')
     # conn.commit()
@@ -52,35 +54,6 @@ def get_people():
     return jsonify([dict(person) for person in people])
 
 
-@app.route('/people', methods=['POST'])
-def add_person():
-    data = request.json
-    conn = get_db_connection()
-    cursor = conn.execute('INSERT INTO People (first_name, last_name) VALUES (?, ?) RETURNING person_id', (data['first_name'], data['last_name']))
-    s = cursor.fetchall()
-    conn.close()
-    return jsonify({'status': 'success', 'created_id': s[0]})
-
-
-@app.route('/people/<int:person_id>', methods=['PUT'])
-def update_person(person_id):
-    data = request.json
-    conn = get_db_connection()
-    conn.execute('UPDATE People SET first_name = ?, last_name = ? WHERE person_id = ?', (data['first_name'], data['last_name'], person_id))
-    conn.commit()
-    conn.close()
-    return jsonify({'status': 'success'})
-
-
-@app.route('/people/<int:person_id>', methods=['DELETE'])
-def delete_person(person_id):
-    conn = get_db_connection()
-    conn.execute('DELETE FROM People WHERE person_id = ?', (person_id,))
-    conn.commit()
-    conn.close()
-    return jsonify({'status': 'success'})
-
-
 # CRUD operations for Emails
 @app.route('/emails/<int:person_id>', methods=['GET'])
 def get_emails(person_id):
@@ -90,29 +63,78 @@ def get_emails(person_id):
     return jsonify([dict(email) for email in emails])
 
 
-@app.route('/emails', methods=['POST'])
-def add_email():
-    data = request.json
-    conn = get_db_connection()
-    conn.execute('INSERT INTO Emails (person_id, email) VALUES (?, ?)', (data['person_id'], data['email']))
-    conn.commit()
-    conn.close()
-    return jsonify({'status': 'success'})
-
-
-@app.route('/emails', methods=['DELETE'])
-def delete_email():
-    data = request.json
-    conn = get_db_connection()
-    conn.execute('DELETE FROM Emails WHERE person_id = ? AND email = ?', (data['person_id'], data['email']))
-    conn.commit()
-    conn.close()
-    return jsonify({'status': 'success'})
-
-
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
+
+
+@app.route('/', methods=['DELETE'])
+def form_handle_delete():
+    # Get request data
+    input_data = request.json
+    print("DELETE:", input_data)
+    person_id = input_data['person_id']
+
+    # Execute deletes from both emails and people tables
+    conn = get_db_connection()
+    conn.execute('DELETE FROM People WHERE person_id = ?', (person_id,))
+    conn.commit()
+
+    conn.execute('DELETE FROM Emails WHERE person_id = ?', (person_id,))
+    conn.commit()
+    conn.close()
+
+    return jsonify({'status': 'success'})
+
+
+@app.route('/', methods=['POST'])
+def form_handle_save():
+    # Get form input data
+    form_data = request.form
+    print(form_data)
+    form_person_id = form_data.get('person_id', "")
+    print(form_person_id)
+    conn = get_db_connection()
+    if form_person_id == "":
+        # If no person_id was passed, this is a new contact
+        conn = get_db_connection()
+
+        cursor = conn.execute('INSERT INTO People (first_name, last_name) VALUES (?, ?) RETURNING person_id',
+                              (form_data['first_name'], form_data['last_name']))
+        s = cursor.fetchall()
+        new_person_id = s[0][0]
+        conn.commit()
+        conn.close()
+
+        # Also add emails if they are present
+        insert_emails(new_person_id, form_data.getlist("inputs[]"))
+    else:
+        conn.execute('UPDATE People SET first_name = ?, last_name = ? WHERE person_id = ?',
+                     (form_data.get('first_name'), form_data.get('last_name'), form_person_id,))
+        conn.commit()
+
+        # Delete and recreate emails in Emails table
+        conn.execute('DELETE FROM Emails WHERE person_id = ?', (form_person_id,))
+        conn.commit()
+        conn.close()
+
+        # Now insert all the emails given as if they were new
+        insert_emails(form_person_id, form_data.getlist("inputs[]"))
+
+    print('done')
+    return jsonify({'status': 'success'})
+
+
+def insert_emails(person_id: int, form_emails: List = None):
+    conn = get_db_connection()
+    if form_emails:
+        insert_sql = 'INSERT INTO Emails (person_id, email) VALUES'
+        for email in form_emails:
+            insert_sql += f' ({person_id}, "{email}"),'
+        insert_sql = insert_sql.rstrip(",")
+        conn.execute(insert_sql)
+        conn.commit()
+    conn.close()
 
 
 
--- flaskapp/static/javascript/crud.js
+++ flaskapp/static/javascript/crud.js
@@ -1,28 +1,26 @@
-let num_emails = 0;
-
 function resetForm() {
-    document.getElementById("main-form").innerHTML = `
-        <input style="display:none;" id="person_id" type="text" value=""/>
-
+    document.getElementById("top-form-container").innerHTML = `
         <div id="name-input-container">
             <div style="float:left;margin-right:20px;">
                 <label class="name-input-label" for="first_name">First Name</label>
-                <input class="name-form-text-input" id="first_name" type="text" />
+                <input class="name-form-text-input" id="first_name" name="first_name" type="text" />
             </div>
 
             <div style="float:left;margin-right:20px;">
                 <label class="name-input-label" for="last_name">Last Name</label>
-                <input class="name-form-text-input" id="last_name" type="text" />
+                <input class="name-form-text-input" id="last_name" name="last_name" type="text" />
             </div>
         </div>
         <div id="email-list-main-container">
             <label class="name-input-label">Email</label>
             <div id="email-list"></div>
-            <div id="email-list-add-button-container">
+            <div id="email-list-add-button-container" onclick="addEmail()">
                 <span class="material-symbols-outlined md-24 my-blue">add_circle</span>
                 <span id="add-email-text" class="my-blue">add email</span>
             </div>
         </div>`
+
+       document.getElementById("person_id").value = "";
 }
 
 async function loadItems() {
@@ -42,6 +40,7 @@ async function loadItems() {
 }
 
 async function addEventListeners() {
+    // Add name listeners
     document.querySelectorAll("#peopleTable tr").forEach(row => {
         row.addEventListener("click", async function () {
             let personId = this.getAttribute("data-id");
@@ -51,19 +50,33 @@ async function addEventListeners() {
                 .then(response => response.json())
                 .then(data => {
                     // Successful request: Update the righthand-side form elements
-                    let firstNameFormInp = document.getElementById("first_name");
-                    firstNameFormInp.value = first;
-                    let lastNameFormInp = document.getElementById("last_name");
-                    lastNameFormInp.value = last;
+                    document.getElementById("person_id").value = personId;
+                    document.getElementById("first_name").value = first;
+                    document.getElementById("last_name").value = last;
 
                     let emailDetailDiv = document.getElementById("email-list");
+                    emailDetailDiv.innerHTML = "";
                     for (let i=0; i<data.length; i++) {
-                        emailDetailDiv.innerHTML = `<input type="text" class="emailTextInput" name="inputs[]" value="${data[i].email}" readonly>`;
+                        let newEmailInput = document.createElement('div')
+                        newEmailInput.setAttribute("class", "email-form-input-container")
+                        newEmailInput.innerHTML = `
+                        <input type="text" class="email-text-input" name="inputs[]" value="${data[i].email}" readonly>
+                        <button type="button" class="email-text-input-delete-button" onclick="removeEmail(this);">
+                        <span class="material-symbols-outlined md-24 my-red">do_not_disturb_on</span>
+                        </button>`;
+
+                        emailDetailDiv.appendChild(newEmailInput);
                     }
                 })
                 .catch(error => console.error("Error fetching person details:", error));
         });
     });
+
+    // Remove default form behavior
+    const form = document.getElementById('main-form');
+    form.addEventListener('submit', async (event) => {
+        event.preventDefault();
+    })
 }
 
 async function addEmail() {
@@ -71,14 +84,52 @@ async function addEmail() {
     let emailDetailDiv = document.getElementById("email-list");
     let newInputContainer = document.createElement('div');
     newInputContainer.setAttribute("class", "email-form-input-container")
-    newInputContainer.innerHTML = `<input type="text" class="email-text-input" name="inputs[]" placeholder="type here..."><button type="button" class="email-text-input-delete-button" onclick="removeEmail(this)"><span class="material-symbols-outlined md-24 my-red">do_not_disturb_on</span></button>`;
+    newInputContainer.innerHTML = `<input type="text" class="email-text-input" name="inputs[]" placeholder="type here..."><button type="button" class="email-text-input-delete-button" onclick="removeEmail(this);"><span class="material-symbols-outlined md-24 my-red">do_not_disturb_on</span></button>`;
     emailDetailDiv.appendChild(newInputContainer);
 }
 
-async function removeEmail(e) {
-    // Remove email from the form
-    console.log(e)
+async function removeEmail(btn) {
+    // Remove the email container from the form
+    btn.parentNode.parentNode.removeChild(btn.parentNode);
 }
 
-// Alright now do stuff
+async function submitSave() {
+    const form = document.getElementById('main-form');
+    const formData = new FormData(form);
+    try {
+        const response = await fetch('/', {
+            method: 'POST',
+            body: formData
+        });
+        if (!response.ok) {
+            console.error('Error submitting form.');
+        }
+    } catch (error) {
+        console.error('There was an error:', error);
+    }
+
+    // Reset the form elements & refresh the contact names sidebar
+    resetForm();
+    loadItems();
+}
+
+async function submitDelete() {
+    const person_id = document.getElementById('person_id').value;
+
+    const response = await fetch('/', {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({"person_id": person_id})
+    });
+    if (!response.ok) {
+        console.error('DELETE failed');
+    }
+
+    // Reset the form elements & refresh the contact names sidebar
+    resetForm();
+    loadItems();
+}
+
 window.onload = loadItems;
